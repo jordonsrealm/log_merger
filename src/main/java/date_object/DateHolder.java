211a460e83c8a16c6415c843e4a9508025e1f2bf
Jordon Tijerina <jordontijerina@gmail.com>
2019-04-17 23:12:04 -0400

    Initial commit.

---
+++ src/main/java/date_object/DateHolder.java
@@ -0,0 +1,67 @@
+package date_object;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class DateHolder implements Comparable<DateHolder>{
+
+    private Date dateObject;
+    private String originalString;
+
+
+    public Date getDateObject() {
+        return dateObject;
+    }
+
+    public String getOriginalString() {
+        return originalString;
+    }
+
+    public DateHolder(String strToConvert, String format){
+        String[] parts = strToConvert.split("\\s");
+
+        String assembleDateString;
+
+        if(parts != null && parts.length > 1){
+            assembleDateString = parts[0] + " " + parts[1];
+        } else{
+            assembleDateString = "";
+        }
+
+        if(format == null || format.isEmpty()){
+            format = "yyyy-MM-dd HH:mm:ss,SSS";
+        }
+
+        SimpleDateFormat formatter = new SimpleDateFormat(format);
+
+        Date dateFromString = null;
+        try {
+            dateFromString = formatter.parse(assembleDateString);
+        } catch (ParseException e) {
+            dateFromString = null;
+        }
+
+        this.dateObject     = dateFromString;
+        this.originalString = strToConvert;
+
+        System.out.println(dateObject);
+        System.out.println(originalString);
+    }
+
+    @Override
+    public String toString() {
+        return "DateHolder [dateObject=" + dateObject.toString() + "]";
+    }
+
+    @Override
+    public int compareTo(DateHolder o) {
+        if(this.getDateObject() == null){
+            return 0;
+        }else{
+            return this.getDateObject().compareTo(o.getDateObject());
+        }
+
+    }
+}
---
+++ src/main/java/merge/Merger.java
@@ -0,0 +1,86 @@
+package merge;
+
+import date_object.DateHolder;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Merger {
+
+    private static JButton mergeBtn;
+    private static JTextArea format;
+    private static JTextArea unOrganizedText;
+    private static JTextArea organizedText;
+
+
+    public static void main(String[] args){
+        JFrame frame = new JFrame("Merge Files");
+
+        frame.setLayout(new BorderLayout());
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+
+        mergeBtn = new JButton("Merge Files");
+        format = new JTextArea(1, 50);
+        unOrganizedText = new JTextArea(1, 45);
+        organizedText   = new JTextArea(1,45);
+        unOrganizedText.setPreferredSize(new Dimension(60,100));
+        organizedText.setPreferredSize(new Dimension(60,100));
+
+        panel.add(unOrganizedText, BorderLayout.WEST);
+        panel.add(organizedText,   BorderLayout.EAST);
+
+        mergeBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String inputStr = unOrganizedText.getText();
+
+                Reader inputString = new StringReader(inputStr);
+
+                BufferedReader bufferedReader = new BufferedReader(inputString);
+                String lineRead = "";
+                StringBuilder builder = new StringBuilder();
+
+                ArrayList<DateHolder> dateHolder = new ArrayList<DateHolder>();
+
+                try {
+                    while((lineRead = bufferedReader.readLine()) != null){
+                        dateHolder.add(new DateHolder(lineRead, format.getText()));
+                    }
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+
+                Collections.sort(dateHolder);
+
+                for(DateHolder holder: dateHolder){
+                    builder.append(holder.getOriginalString() + "\n");
+                }
+
+                organizedText.setText(builder.toString());
+            }
+        });
+
+        JPanel topPanel = new JPanel();
+        topPanel.setLayout(new BorderLayout());
+        topPanel.add(mergeBtn, BorderLayout.NORTH);
+        format.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        topPanel.add(format, BorderLayout.SOUTH);
+
+        frame.add(topPanel, BorderLayout.NORTH);
+        frame.add(panel, BorderLayout.CENTER);
+
+        frame.setVisible(true);
+        frame.setResizable(false);
+        frame.setSize(new Dimension(1000,600));
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    }
+}
